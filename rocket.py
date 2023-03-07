58a8e899cc08bff8a6196d4f97cab23a9025508e
Lance Dizon <lance.dizon@ucalgary.ca>
2023-03-07 11:08:06 -0700

    edited rocket

--- rocket.py
+++ rocket.py
@@ -13,11 +13,21 @@ def printRocket():
                 /       \\
                     
             """ )
+    
+def printPlatform():
+    print(
+        """
+
+        ___________________________
+
+        """ )
+ 
+
 printRocket()
+printPlatform()
 
 delay = 300
 for i in range(60):
     print()
     sleep(delay/1000)
     delay = delay * 0.9
-    
